155f61e3bc69bcb622b5c36b0270827a4764c55a
junghyeonsu <jung660317@naver.com>
2023-05-16 17:05:10 +0900

    feat(figma-plugin): add chakra

--- packages/figma-plugin/ui-src/main.tsx
+++ packages/figma-plugin/ui-src/main.tsx
@@ -2,9 +2,14 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./pages/App";
 
+import { ChakraProvider } from '@chakra-ui/react';
+
+
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 );
--- packages/figma-plugin/ui-src/pages/App.tsx
+++ packages/figma-plugin/ui-src/pages/App.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import "../ui.css";
 import { appContainer } from "./App.css";
+import { Input } from '@chakra-ui/react';
 
 const App = ({}) => {
   return (
     <div className={appContainer}>
       hello world
       hi
+      di
+      <Input placeholder='Basic usage' />
     </div>
   );
 };
