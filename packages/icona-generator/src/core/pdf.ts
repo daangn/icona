902271500a84859f76b57ec2bbb340af0c892d48
junghyeonsu <jung660317@naver.com>
2023-09-07 12:34:26 +0900

    feat: delete CreationDate

--- packages/icona-generator/src/core/pdf.ts
+++ packages/icona-generator/src/core/pdf.ts
@@ -38,6 +38,15 @@ export const generatePDF = ({
 
     const svgPath = resolve(projectPath, path, `${name}.pdf`);
     const pdfDoc = new PDFDocument(pdfkitConfig);
+
+    /**
+     * @description Remove PDF creation date
+     * because it cause the PDF to be different every time
+     *
+     * @see https://github.com/foliojs/pdfkit/blob/4ec77ddc8c090c8d0d57fbd72cff433e9ce0d733/docs/getting_started.md?plain=1#L194
+     */
+    pdfDoc.info.CreationDate = new Date(0);
+
     SVGtoPDF(pdfDoc, svg, x, y, restSvgToPdfOptions);
     pdfDoc.pipe(createWriteStream(svgPath));
     pdfDoc.end();
